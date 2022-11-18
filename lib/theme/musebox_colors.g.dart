319b2642d813fd5f55522196e195772ab0517607
Parabeac-Nest <admin@parabeac.com>
2022-11-18 04:46:30 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/musebox_colors.g.dart
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class MuseboxColors {
+
+static const Color pRIMGris = Color(0xff6f6f6f);
+
+static const Color pRIMNaranja = Color(0xfff24e1e);
+
+static const Color sECVerde = Color(0xff24a63a);
+
+static const Color labelColorDarkPrimary = Color(0xffffffff);
+
+static const Color sECAzul = Color(0xff0378a6);
+
+static const Color pRIMNegro = Color(0xff1e1e1e);
+
+static const Color labelColorLightPrimary = Color(0xff000000);
+}
---
+++ lib/theme/musebox_text_styles.g.dart
@@ -0,0 +1,120 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class MuseboxTextStyles {
+
+static const TextStyle diminuto = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Anton',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle defaultBoldBody = TextStyle(
+  fontSize: 17,
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.40799999237060547,
+  fontFamily: 'SF Pro Text',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle pequeno = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Anton',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle textoGran = TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Anton',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle parrafo = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Anton',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle defaultRegularTitle2 = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.3499999940395355,
+  fontFamily: 'SF Pro Display',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle defaultRegularCallout = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.3199999928474426,
+  fontFamily: 'SF Pro Text',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle tituloBold = TextStyle(
+  fontSize: 48,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'COCOGOOSE',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle titulo = TextStyle(
+  fontSize: 48,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Cocogoose',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle subTituloBold = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'COCOGOOSE',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle subTitulo = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Cocogoose',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
